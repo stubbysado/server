87b41fc9b80dad077c4c2494c37d116042b4cc7a
stubbysado <114629686+stubbysado@users.noreply.github.com>
2022-09-30 20:47:22 +0800

    Update post-install-server.sh

--- post-install-server.sh
+++ post-install-server.sh
@@ -117,9 +117,7 @@ sudo systemctl start transmission-daemon.service
 sudo systemctl stop transmission-daemon.service
 
 sudo sed -i 's|"download-dir": "/home/oggy/Downloads",|"download-dir": "/mnt/server/02 Downloads/Transmission",|g' /home/oggy/.config/transmission-daemon/settings.json
-
 sudo sed -i 's|"rpc-username": "",|"rpc-username": "oggy",|g' /home/oggy/.config/transmission-daemon/settings.json
-
 sudo sed -i 's|"rpc-whitelist": "127.0.0.1,::1",|"rpc-whitelist": "127.0.0.1,192.168.*.*",|g' /home/oggy/.config/transmission-daemon/settings.json
 
 sudo systemctl daemon-reload
