265a9ce1163db9cd3f7c477532e5fa34c4504877
stubbysado <114629686+stubbysado@users.noreply.github.com>
2023-01-01 19:40:20 +0800

    Update post-install-server2.sh

--- post-install-server2.sh
+++ post-install-server2.sh
@@ -22,26 +22,17 @@ sudo systemctl mask systemd-networkd-wait-online.service
 # FSTAB
 sudo mkdir /mnt/parity1
 sudo mkdir /mnt/data1
-sudo mkdir /mnt/data2
-sudo mkdir /mnt/data3
-sudo mkdir /mnt/data4
 
 echo '' | sudo tee -a /etc/fstab
 echo '# Hard Disk Drive' | sudo tee -a /etc/fstab
-echo 'UUID=1326ca8d-bae5-442f-8abd-ce838a1eb5e3 /mnt/parity1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
+echo 'UUID=6e10f55a-2d5d-4a42-a636-5cb42ea1bd05 /mnt/parity1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
 echo '' | sudo tee -a /etc/fstab
-echo 'UUID=bb3142a8-5b8d-4b99-b27f-166b9cb1060d /mnt/data1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=5ce59e64-be1a-4235-a291-b94b8217667e /mnt/data2 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=2e8f3d3d-1664-4cf4-9a2f-d59834aa0315 /mnt/data3 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=b365421e-db36-4faf-9816-14503d7685d7 /mnt/data4 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
+echo 'UUID=fbd9d55a-1ed1-47c9-8a8a-58ed3769fc70 /mnt/data1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
 
 sudo mount -a
 
 sudo chown oggy:oggy /mnt/parity1
 sudo chown oggy:oggy /mnt/data1
-sudo chown oggy:oggy /mnt/data2
-sudo chown oggy:oggy /mnt/data3
-sudo chown oggy:oggy /mnt/data4
 
 # MERGERFS
 sudo mkdir /mnt/server2
@@ -92,14 +83,8 @@ sudo make install
 echo 'parity /mnt/parity1/snapraid.parity' | sudo tee -a /etc/snapraid.conf
 echo '' | sudo tee -a /etc/snapraid.conf
 echo 'content /mnt/data1/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data2/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data3/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data4/snapraid.content' | sudo tee -a /etc/snapraid.conf
 echo '' | sudo tee -a /etc/snapraid.conf
 echo 'data data1 /mnt/data1/' | sudo tee -a /etc/snapraid.conf
-echo 'data data2 /mnt/data2/' | sudo tee -a /etc/snapraid.conf
-echo 'data data3 /mnt/data3/' | sudo tee -a /etc/snapraid.conf
-echo 'data data4 /mnt/data4/' | sudo tee -a /etc/snapraid.conf
 echo '' | sudo tee -a /etc/snapraid.conf
 echo 'exclude /lost+found/' | sudo tee -a /etc/snapraid.conf
 
