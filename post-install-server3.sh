c3ee90a5e078a7da00d72dcecbdbfe80db206d45
stubbysado <114629686+stubbysado@users.noreply.github.com>
2023-01-17 14:08:27 +0800

    Update post-install-server3.sh

--- post-install-server3.sh
+++ post-install-server3.sh
@@ -37,11 +37,9 @@ sudo chown oggy:oggy /mnt/data1
 # MERGERFS
 sudo mkdir /mnt/server3
 
-sudo mergerfs -o use_ino,cache.files=full,dropcacheonclose=true,allow_other,category.create=mfs,fsname=server3 /mnt/data* /mnt/server3
-
 echo '' | sudo tee -a /etc/fstab
 echo '# MergerFS' | sudo tee -a /etc/fstab
-echo '/mnt/data* /mnt/server3 fuse.mergerfs use_ino,cache.files=full,dropcacheonclose=true,allow_other,category.create=mfs,fsname=server3,nonempty 0 0' | sudo tee -a /etc/fstab
+echo '/mnt/data* /mnt/server3 fuse.mergerfs allow_other,cache.files=full,dropcacheonclose=true,category.create=mfs 0 0' | sudo tee -a /etc/fstab
 
 sudo chown oggy:oggy /mnt/server3
 
