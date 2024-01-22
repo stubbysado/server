9efe046d78feee9922e5a62420677551c3e59777
stubbysado <114629686+stubbysado@users.noreply.github.com>
2024-01-23 01:57:40 +0800

    Update post-install-server2.sh

--- post-install-server2.sh
+++ post-install-server2.sh
@@ -87,7 +87,7 @@ echo -e "$PASSWORD\n$PASSWORD" | sudo smbpasswd -a $(whoami)
 sudo systemctl restart smbd.service
 
 # SNAPRAID
-mkdir /home/oggy/snapraid/
+mkdir /home/oggy/snapraid
 wget https://github.com/amadvance/snapraid/releases/download/v12.2/snapraid-12.2.tar.gz -P /home/oggy/snapraid/
 
 tar -xzf /home/oggy/snapraid/snapraid-12.2.tar.gz -C /home/oggy/snapraid/
