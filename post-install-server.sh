d4c7bf853d04edff92cca4ef3c623a89334577e3
stubbysado <114629686+stubbysado@users.noreply.github.com>
2024-01-23 02:08:10 +0800

    Update post-install-server.sh

--- post-install-server.sh
+++ post-install-server.sh
@@ -166,14 +166,5 @@ CRONJOB="0 0 * * * /home/oggy/runner.sh"
 
 cat <(fgrep -i -v "$CRONTSCRIPT" <(crontab -l)) <(echo "$CRONJOB") | crontab -
 
-#DOWNLOAD EXTRA SCRIPTS
-wget https://raw.githubusercontent.com/stubbysado/extras/main/update.sh -P /home/oggy/
-wget https://raw.githubusercontent.com/stubbysado/extras/main/poweroff.sh -P /home/oggy/
-wget https://raw.githubusercontent.com/stubbysado/extras/main/reboot.sh -P /home/oggy/
-wget https://raw.githubusercontent.com/stubbysado/extras/main/sync.sh -P /home/oggy/
-wget https://raw.githubusercontent.com/stubbysado/extras/main/scrub.sh -P /home/oggy/
-sudo chmod 775 -v /home/oggy/*.sh
-sudo chown oggy:oggy -v /home/oggy/*.sh
-
 # SYNC
 sync && sync
