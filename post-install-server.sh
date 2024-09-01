81e8dc2f309b77d479180283f3ef90dff70afcbc
stubbysado <114629686+stubbysado@users.noreply.github.com>
2024-09-01 21:42:30 +0800

    Update post-install-server.sh

--- post-install-server.sh
+++ post-install-server.sh
@@ -154,13 +154,6 @@ sudo systemctl restart transmission-daemon.service
 
 # PRINTER
 sudo cp /etc/cups/cupsd.conf /etc/cups/cupsd.conf.bak
-
-sudo sed -i 's|Listen localhost:631|Port 631|g' /etc/cups/cupsd.conf
-sudo sed -i 's|Browsing No|Browsing Yes|g' /etc/cups/cupsd.conf
-sudo sed -i '37i Allow @LOCAL' /etc/cups/cupsd.conf
-sudo sed -i '43i Allow @LOCAL' /etc/cups/cupsd.conf
-sudo sed -i 's|Allow @LOCAL|  Allow @LOCAL|g' /etc/cups/cupsd.conf
-
 sudo usermod -aG lpadmin oggy
 sudo systemctl restart cups
 
