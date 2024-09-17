70b4a89e82d0b7f0c8786a68c768a6aab5d220b9
stubbysado <114629686+stubbysado@users.noreply.github.com>
2024-09-17 08:35:25 +0800

    Update post-install-server2.sh

--- post-install-server2.sh
+++ post-install-server2.sh
@@ -110,7 +110,7 @@ echo 'data data2 /mnt/data2/' | sudo tee -a /etc/snapraid.conf
 echo 'data data3 /mnt/data3/' | sudo tee -a /etc/snapraid.conf
 echo 'data data4 /mnt/data4/' | sudo tee -a /etc/snapraid.conf
 echo '' | sudo tee -a /etc/snapraid.conf
-echo 'exclude /lost+found/' | sudo tee -a /etc/snapraid.conf
+echo 'exclude lost+found/' | sudo tee -a /etc/snapraid.conf
 
 # EXTRA HDD
 sudo mkdir /mnt/ext1
