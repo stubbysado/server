712fcf873216257929d2625e183f20777a1319f7
stubbysado <114629686+stubbysado@users.noreply.github.com>
2025-10-27 08:54:11 +0800

    Update post-install-server.sh

--- post-install-server.sh
+++ post-install-server.sh
@@ -6,7 +6,7 @@ sudo apt purge snapd* -y
 # DOWNLOAD PACKAGE
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install cups gcc hplip make mergerfs samba transmission-daemon -y
+sudo apt install cups gcc hplip make mc mergerfs samba transmission-daemon -y
 
 # RE-CHECK UPDATE
 sudo apt update -y
@@ -25,7 +25,7 @@ sudo systemctl mask systemd-networkd-wait-online.service
 # FSTAB
 sudo mkdir /mnt/parity1
 sudo mkdir /mnt/data1
-sudo mkdir /mnt/data2
+sudo mkdir /mnt/data2a1
 sudo mkdir /mnt/data3
 sudo mkdir /mnt/data4
 sudo mkdir /mnt/data5
@@ -38,7 +38,7 @@ echo '# Hard Disk Drive' | sudo tee -a /etc/fstab
 echo 'UUID=80274962-7f78-4935-884a-c1ef00aba684 /mnt/parity1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
 echo '' | sudo tee -a /etc/fstab
 echo 'UUID=c180a0f4-c1fa-4d14-a811-32070222e595 /mnt/data1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=ba563c40-37ea-4a95-85a9-ba3111199db1 /mnt/data2 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
+echo 'UUID=3cd34620-4876-4f14-90cc-8260281baf4b /mnt/data2a1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
 echo 'UUID=06765f35-5626-403a-9190-0872d22edb8d /mnt/data3 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
 echo 'UUID=28fd0102-f269-43bb-88a6-959f7ea9dc65 /mnt/data4 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
 echo 'UUID=248d2eb9-6330-402d-a620-a74974b29af7 /mnt/data5 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
@@ -50,7 +50,7 @@ sudo mount -a
 
 sudo chown oggy:oggy /mnt/parity1
 sudo chown oggy:oggy /mnt/data1
-sudo chown oggy:oggy /mnt/data2
+sudo chown oggy:oggy /mnt/data2a1
 sudo chown oggy:oggy /mnt/data3
 sudo chown oggy:oggy /mnt/data4
 sudo chown oggy:oggy /mnt/data5
@@ -111,14 +111,14 @@ rm -rfv /home/oggy/snapraid
 echo 'parity /mnt/parity1/snapraid.parity' | sudo tee -a /etc/snapraid.conf
 echo '' | sudo tee -a /etc/snapraid.conf
 echo 'content /mnt/data1/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data2/snapraid.content' | sudo tee -a /etc/snapraid.conf
+echo 'content /mnt/data2a1/snapraid.content' | sudo tee -a /etc/snapraid.conf
 echo 'content /mnt/data3/snapraid.content' | sudo tee -a /etc/snapraid.conf
 echo 'content /mnt/data4/snapraid.content' | sudo tee -a /etc/snapraid.conf
 echo 'content /mnt/data5/snapraid.content' | sudo tee -a /etc/snapraid.conf
 echo 'content /mnt/data6/snapraid.content' | sudo tee -a /etc/snapraid.conf
 echo '' | sudo tee -a /etc/snapraid.conf
 echo 'data data1 /mnt/data1/' | sudo tee -a /etc/snapraid.conf
-echo 'data data2 /mnt/data2/' | sudo tee -a /etc/snapraid.conf
+echo 'data data2 /mnt/data2a1/' | sudo tee -a /etc/snapraid.conf
 echo 'data data3 /mnt/data3/' | sudo tee -a /etc/snapraid.conf
 echo 'data data4 /mnt/data4/' | sudo tee -a /etc/snapraid.conf
 echo 'data data5 /mnt/data5/' | sudo tee -a /etc/snapraid.conf
@@ -132,21 +132,28 @@ echo 'exclude snapraid.log' | sudo tee -a /etc/snapraid.conf
 echo 'exclude snapraid-output.log' | sudo tee -a /etc/snapraid.conf
 
 # TRANSMISSION
-# sudo systemctl stop transmission-daemon.service
-# sudo cp /lib/systemd/system/transmission-daemon.service /lib/systemd/system/transmission-daemon.service.bak
-# sudo sed -i 's|User=debian-transmission|User=oggy|g' /lib/systemd/system/transmission-daemon.service
+sudo systemctl stop transmission-daemon.service
+sudo systemctl start transmission-daemon.service
+sudo systemctl stop transmission-daemon.service
+sleep 3
+sudo cp /lib/systemd/system/transmission-daemon.service /lib/systemd/system/transmission-daemon.service.bak
+sudo sed -i 's|User=debian-transmission|User=oggy|g' /lib/systemd/system/transmission-daemon.service
 # Temporary workaround
-# sudo sed -i 's|Type=notify|Type=simple|g' /lib/systemd/system/transmission-daemon.service
-# sudo systemctl daemon-reload
-# sudo systemctl restart transmission-daemon.service
-# sudo systemctl stop transmission-daemon.service
-# sudo cp /home/oggy/.config/transmission-daemon/settings.json /home/oggy/.config/transmission-daemon/settings.json.bak
-# sudo sed -i 's|"download-dir": "/home/oggy/Downloads",|"download-dir": "/mnt/server/02-Downloads/Transmission",|g' /home/oggy/.config/transmission-daemon/settings.json
-# sudo sed -i 's|"rpc-authentication-required": false,|"rpc-authentication-required": true,|g' /home/oggy/.config/transmission-daemon/settings.json
-# sudo sed -i 's|"rpc-username": "",|"rpc-username": "oggy",|g' /home/oggy/.config/transmission-daemon/settings.json
-# sudo sed -i 's|"rpc-whitelist": "127.0.0.1,::1",|"rpc-whitelist": "127.0.0.1,10.0.0.*",|g' /home/oggy/.config/transmission-daemon/settings.json
-# sudo systemctl daemon-reload
-# sudo systemctl restart transmission-daemon.service
+sudo sed -i 's|Type=notify|Type=simple|g' /lib/systemd/system/transmission-daemon.service
+sudo systemctl daemon-reload
+sudo systemctl start transmission-daemon.service
+sleep 3
+sudo systemctl stop transmission-daemon.service
+sudo systemctl start transmission-daemon.service
+sudo systemctl stop transmission-daemon.service
+sleep 3
+sudo cp /home/oggy/.config/transmission-daemon/settings.json /home/oggy/.config/transmission-daemon/settings.json.bak
+sudo sed -i 's|"download-dir": "/home/oggy/Downloads",|"download-dir": "/mnt/server/02-Downloads/Transmission",|g' /home/oggy/.config/transmission-daemon/settings.json
+sudo sed -i 's|"rpc-authentication-required": false,|"rpc-authentication-required": true,|g' /home/oggy/.config/transmission-daemon/settings.json
+sudo sed -i 's|"rpc-username": "",|"rpc-username": "oggy",|g' /home/oggy/.config/transmission-daemon/settings.json
+sudo sed -i 's|"rpc-whitelist": "127.0.0.1,::1",|"rpc-whitelist": "127.0.0.1,10.0.0.*",|g' /home/oggy/.config/transmission-daemon/settings.json
+sudo systemctl daemon-reload
+sudo systemctl start transmission-daemon.service
 
 # PRINTER
 sudo cp /etc/cups/cupsd.conf /etc/cups/cupsd.conf.bak
