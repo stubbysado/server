a76bfc51517d46fb1b7304da30f3a0ba07b7a023
stubbysado <114629686+stubbysado@users.noreply.github.com>
2025-11-04 09:29:53 +0800

    Update server2-debian.sh

--- server2-debian.sh
+++ server2-debian.sh
@@ -16,7 +16,7 @@ echo 'Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg' | sudo tee -a /
 # INSTALL PACKAGES
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install htop make mergerfs samba screen -y
+sudo apt install gcc htop make mergerfs samba screen -y
 
 # RE-CHECK UPDATE
 sudo apt update -y
