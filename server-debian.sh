0df665cf456f96cb7ce301d7ca88abcdf75e526e
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-02-01 12:41:53 +0800

    Update server-debian.sh

--- server-debian.sh
+++ server-debian.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -x
 
 # SOURCES.LIST
-echo 'Types: deb deb-src' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'URIs: https://mirror.sg.gs/debian' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'Suites: trixie trixie-updates' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'Components: main contrib non-free non-free-firmware' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo '' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'Types: deb deb-src' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'URIs: https://mirror.sg.gs/debian-security' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'Suites: trixie-security' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'Components: main contrib non-free non-free-firmware' | sudo tee -a /etc/apt/sources.list.d/debian.sources
-echo 'Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg' | sudo tee -a /etc/apt/sources.list.d/debian.sources
+sudo tee /etc/apt/sources.list.d/debian.sources <<EOF
+Types: deb deb-src
+URIs: https://mirror.sg.gs/debian
+Suites: trixie trixie-updates
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+Types: deb deb-src
+URIs: https://mirror.sg.gs/debian-security
+Suites: trixie-security
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+EOF
 
 # INSTALL PACKAGES
 sudo apt update -y
@@ -25,111 +27,97 @@ sudo apt clean -y
 sudo apt autoremove -y
 
 # FSTAB
-sudo mkdir /mnt/parity1
-sudo mkdir /mnt/data1
-sudo mkdir /mnt/data2
-sudo mkdir /mnt/data3
-sudo mkdir /mnt/data4
-sudo mkdir /mnt/data5
-sudo mkdir /mnt/data6
-
+sudo mkdir -p /mnt/parity1 /mnt/data{1..6}
 sudo cp /etc/fstab /etc/fstab.bak
 
-echo '' | sudo tee -a /etc/fstab
-echo '# Hard Disk Drive' | sudo tee -a /etc/fstab
-echo 'UUID=80274962-7f78-4935-884a-c1ef00aba684 /mnt/parity1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo '' | sudo tee -a /etc/fstab
-echo 'UUID=c180a0f4-c1fa-4d14-a811-32070222e595 /mnt/data1 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=3cd34620-4876-4f14-90cc-8260281baf4b /mnt/data2 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=06765f35-5626-403a-9190-0872d22edb8d /mnt/data3 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=28fd0102-f269-43bb-88a6-959f7ea9dc65 /mnt/data4 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=248d2eb9-6330-402d-a620-a74974b29af7 /mnt/data5 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
-echo 'UUID=64ebf1c2-a790-465d-84f7-b0eff587e446 /mnt/data6 auto nosuid,nodev,nofail 0 0' | sudo tee -a /etc/fstab
+sudo tee -a /etc/fstab <<EOF
+
+# Hard Disk Drive
+UUID=80274962-7f78-4935-884a-c1ef00aba684 /mnt/parity1 auto nosuid,nodev,nofail 0 0
+UUID=c180a0f4-c1fa-4d14-a811-32070222e595 /mnt/data1   auto nosuid,nodev,nofail 0 0
+UUID=3cd34620-4876-4f14-90cc-8260281baf4b /mnt/data2   auto nosuid,nodev,nofail 0 0
+UUID=06765f35-5626-403a-9190-0872d22edb8d /mnt/data3   auto nosuid,nodev,nofail 0 0
+UUID=28fd0102-f269-43bb-88a6-959f7ea9dc65 /mnt/data4   auto nosuid,nodev,nofail 0 0
+UUID=248d2eb9-6330-402d-a620-a74974b29af7 /mnt/data5   auto nosuid,nodev,nofail 0 0
+UUID=64ebf1c2-a790-465d-84f7-b0eff587e446 /mnt/data6   auto nosuid,nodev,nofail 0 0
+EOF
 
 sudo systemctl daemon-reload
-
 sudo mount -a
-
-sudo chown oggy:oggy /mnt/parity1
-sudo chown oggy:oggy /mnt/data1
-sudo chown oggy:oggy /mnt/data2
-sudo chown oggy:oggy /mnt/data3
-sudo chown oggy:oggy /mnt/data4
-sudo chown oggy:oggy /mnt/data5
-sudo chown oggy:oggy /mnt/data6
+sudo chown oggy:oggy /mnt/parity1 /mnt/data{1..6}
 
 # MERGERFS
-sudo mkdir /mnt/server
+sudo mkdir -p /mnt/server
 
-echo '' | sudo tee -a /etc/fstab
-echo '# MergerFS' | sudo tee -a /etc/fstab
-echo '/mnt/data* /mnt/server mergerfs cache.files=off,category.create=pfrd,func.getattr=newest,dropcacheonclose=false 0 0' | sudo tee -a /etc/fstab
+sudo tee -a /etc/fstab <<EOF
+
+# MergerFS
+/mnt/data* /mnt/server mergerfs cache.files=off,category.create=pfrd,func.getattr=newest,dropcacheonclose=false 0 0
+EOF
 
 sudo systemctl daemon-reload
-
 sudo mount -a
-
 sudo chown oggy:oggy /mnt/server
 
 # SAMBA
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
 
-echo '[server]' | sudo tee -a /etc/samba/smb.conf
-echo 'path = /mnt/server' | sudo tee -a /etc/samba/smb.conf
-echo 'browseable = no' | sudo tee -a /etc/samba/smb.conf
-echo 'read only = no' | sudo tee -a /etc/samba/smb.conf
-echo 'guest ok = no' | sudo tee -a /etc/samba/smb.conf
-echo 'valid users = oggy' | sudo tee -a /etc/samba/smb.conf
-echo 'vfs objects = recycle' | sudo tee -a /etc/samba/smb.conf
-echo 'recycle:repository = .recycle' | sudo tee -a /etc/samba/smb.conf
-echo 'recycle:directory_mode = 755' | sudo tee -a /etc/samba/smb.conf
-echo 'recycle:versions = yes' | sudo tee -a /etc/samba/smb.conf
+sudo tee -a /etc/samba/smb.conf <<EOF
+
+[server]
+   path = /mnt/server
+   browseable = no
+   read only = no
+   guest ok = no
+   valid users = oggy
+   vfs objects = recycle
+   recycle:repository = .recycle
+   recycle:directory_mode = 755
+   recycle:versions = yes
+EOF
 
 PASSWORD="sudo"
 
 echo -e "$PASSWORD\n$PASSWORD" | sudo smbpasswd -a oggy
-
 sudo systemctl restart smbd.service
 
 # SNAPRAID
 mkdir /home/oggy/snapraid
-
 wget https://github.com/amadvance/snapraid/releases/download/v13.0/snapraid-13.0.tar.gz -P /home/oggy/snapraid/
-
 tar -xzf /home/oggy/snapraid/snapraid-13.0.tar.gz -C /home/oggy/snapraid/
 
 CONFIGURESNAPRAID="/home/oggy/snapraid/snapraid-13.0/configure"
 
 cd /home/oggy/snapraid && $CONFIGURESNAPRAID
-
 make -C /home/oggy/snapraid
-
 sudo make install
-
 rm -rfv /home/oggy/snapraid
 
-echo 'parity /mnt/parity1/snapraid.parity' | sudo tee -a /etc/snapraid.conf
-echo '' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data1/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data2/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data3/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data4/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data5/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo 'content /mnt/data6/snapraid.content' | sudo tee -a /etc/snapraid.conf
-echo '' | sudo tee -a /etc/snapraid.conf
-echo 'data data1 /mnt/data1/' | sudo tee -a /etc/snapraid.conf
-echo 'data data2 /mnt/data2/' | sudo tee -a /etc/snapraid.conf
-echo 'data data3 /mnt/data3/' | sudo tee -a /etc/snapraid.conf
-echo 'data data4 /mnt/data4/' | sudo tee -a /etc/snapraid.conf
-echo 'data data5 /mnt/data5/' | sudo tee -a /etc/snapraid.conf
-echo 'data data6 /mnt/data6/' | sudo tee -a /etc/snapraid.conf
-echo '' | sudo tee -a /etc/snapraid.conf
-echo 'exclude lost+found/' | sudo tee -a /etc/snapraid.conf
-echo 'exclude .recycle/' | sudo tee -a /etc/snapraid.conf
-echo 'exclude 02-Downloads/' | sudo tee -a /etc/snapraid.conf
-echo 'exclude *.part' | sudo tee -a /etc/snapraid.conf
-echo 'exclude snapraid.log' | sudo tee -a /etc/snapraid.conf
-echo 'exclude snapraid-output.log' | sudo tee -a /etc/snapraid.conf
+sudo tee /etc/snapraid.conf <<EOF
+
+parity /mnt/parity1/snapraid.parity
+
+content /mnt/data1/snapraid.content
+content /mnt/data2/snapraid.content
+content /mnt/data3/snapraid.content
+content /mnt/data4/snapraid.content
+content /mnt/data5/snapraid.content
+content /mnt/data6/snapraid.content
+
+data data1 /mnt/data1/
+data data2 /mnt/data2/
+data data3 /mnt/data3/
+data data4 /mnt/data4/
+data data5 /mnt/data5/
+data data6 /mnt/data6/
+
+exclude lost+found/
+exclude .recycle/
+exclude 02-Downloads/
+exclude *.part
+exclude snapraid.log
+exclude snapraid-output.log
+EOF
 
 # TRANSMISSION
 sudo systemctl stop transmission-daemon.service
@@ -161,12 +149,11 @@ CRONJOB="#0 0 * * * /home/oggy/runner.sh"
 cat <(fgrep -i -v "$CRONTSCRIPT" <(crontab -l)) <(echo "$CRONJOB") | crontab -
 
 # ALIAS
-echo "alias ll='ls -la'" | sudo tee -a /home/oggy/.bashrc
+echo "alias ll='ls -la'" >> /home/oggy/.bashrc
 
 # WAKE ON LAN
 sudo cp /etc/network/interfaces /etc/network/interfaces.bak
-
 echo 'ethernet-wol g' | sudo tee -a /etc/network/interfaces
 
-# SYNC
 sync && sync
+# SYNC
