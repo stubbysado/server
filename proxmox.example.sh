cb3c732bbffb3a8d94de1539786796b5e80e98fe
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-02-01 13:02:57 +0800

    Update proxmox.example.sh

--- proxmox.example.sh
+++ proxmox.example.sh
@@ -5,16 +5,35 @@ LINE="@reboot echo 0 | tee /sys/class/backlight/intel_backlight/brightness"
 
 (crontab -l 2>/dev/null | grep -Fq "$LINE") || (crontab -l 2>/dev/null; echo "$LINE") | crontab -
 
-# SOURCE.LIST.D
-sed -i 's|https://enterprise.proxmox.com|http://download.proxmox.com|g' /etc/apt/sources.list.d/ceph.sources
-sed -i 's|enterprise|no-subscription|g' /etc/apt/sources.list.d/ceph.sources
+# BACKUP REPO
+mkdir -p /root/sources_list_bak/
+mv /etc/apt/sources.list.d/* /root/sources_list_bak/
+mv /etc/apt/sources.list /root/sources_list_bak/sources.list.bak
 
-sed -i 's|https://enterprise.proxmox.com|http://download.proxmox.com|g' /etc/apt/sources.list.d/pve-enterprise.sources
-sed -i 's|pve-enterprise|pve-no-subscription|g' /etc/apt/sources.list.d/pve-enterprise.sources
+# CREATE REPO
+tee /etc/apt/sources.list.d/debian.sources <<EOF
+Types: deb deb-src
+URIs: https://mirror.sg.gs/debian
+Suites: trixie trixie-updates
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
-sed -i 's|http://deb.debian.org|https://mirror.sg.gs|g' /etc/apt/sources.list.d/debian.sources
-sed -i 's|http://security.debian.org|https://mirror.sg.gs|g' /etc/apt/sources.list.d/debian.sources
+Types: deb deb-src
+URIs: https://mirror.sg.gs/debian-security
+Suites: trixie-security
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+EOF
 
+tee /etc/apt/sources.list.d/proxmox.sources <<EOF
+Types: deb
+URIs: http://download.proxmox.com/debian/pve
+Suites: trixie
+Components: pve-no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+EOF
+
+# UPDATE
 apt update
 apt dist-upgrade -y
 apt clean -y
