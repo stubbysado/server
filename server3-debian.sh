1e736e2a22ee291268c7bb21cd22b65a687a21fc
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-02-12 14:12:38 +0800

    Update server3-debian.sh

--- server3-debian.sh
+++ server3-debian.sh
@@ -16,14 +16,14 @@ Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 EOF
 
 # INSTALL PACKAGES
-sudo apt update -y
+sudo apt update
 sudo apt upgrade -y
 sudo apt install gcc htop make mergerfs samba screen -y
 
 # RE-CHECK UPDATE
-sudo apt update -y
+sudo apt update
 sudo apt upgrade -y
-sudo apt clean -y
+sudo apt clean
 sudo apt autoremove -y
 
 # FSTAB
