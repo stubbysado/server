4c83f6c6199156c15289b17ceacf1743b25c912d
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-02-15 12:00:26 +0800

    Update server-debian.sh

--- server-debian.sh
+++ server-debian.sh
@@ -21,7 +21,7 @@ EOF
 # PACKAGES
 sudo apt update
 sudo apt upgrade -y
-sudo apt install gcc htop make mergerfs qemu-guest-agent samba screen -y
+sudo apt install gcc htop make mergerfs samba screen -y
 
 # RE-CHECK UPDATE
 sudo apt update
