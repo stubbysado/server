c46bca955b794f49db6cd3766be4d9cbd5af3ac4
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-02-28 05:56:32 +0800

    Update lxc-nginx-reverse-caching-proxy.sh

--- lxc-nginx-reverse-caching-proxy.sh
+++ lxc-nginx-reverse-caching-proxy.sh
@@ -43,33 +43,26 @@ mkdir -p "$CACHE_DIR"
 chown www-data:www-data "$CACHE_DIR"
 
 tee "$CONF_PATH" <<EOF
-proxy_cache_path $CACHE_DIR 
-    levels=1:2 
-    keys_zone=deb_cache:10m 
-    max_size=5g 
-    inactive=14d 
+proxy_cache_path $CACHE_DIR
+    levels=1:2
+    keys_zone=deb_cache:10m
+    max_size=5g
+    inactive=30d
     use_temp_path=off;
-
 server {
     listen 80;
     server_name $SERVER_IP;
-
     access_log /var/log/nginx/apt-proxy-access.log;
     error_log /var/log/nginx/apt-proxy-error.log;
-
     location / {
         proxy_pass $MIRROR_URL;
         proxy_cache deb_cache;
-        
         proxy_cache_valid 200 302 6h;
-        
         location ~* \.deb$ {
             proxy_pass $MIRROR_URL;
             proxy_cache_valid 200 302 14d;
         }
-
         add_header X-Cache-Status \$upstream_cache_status;
-
         proxy_set_header Host mirror.sg.gs;
         proxy_ssl_server_name on;
         proxy_ssl_verify on;
