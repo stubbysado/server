0610980a8657ef0bc61198ff30c31745b5cb4e35
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-02-28 02:52:56 +0800

    Add files via upload

---
+++ lxc-nginx-reverse-caching-proxy.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# DELETE SOURCES.LIST
+/etc/apt/sources.list.d/debian.sources
+
+# SOURCES.LIST.D
+tee /etc/apt/sources.list.d/debian.sources <<'EOF'
+Types: deb deb-src
+URIs: https://mirror.sg.gs/debian
+Suites: trixie trixie-updates
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+Types: deb deb-src
+URIs: https://mirror.sg.gs/debian-security
+Suites: trixie-security
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+EOF
+
+# UPDATE
+apt update
+apt upgrade -y
+apt clean
+apt autoremove -y
+
+# NGINX REVERSE CACHING PROXY
+set -e
+
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please run as root"
+  exit 1
+fi
+
+CACHE_DIR="/var/cache/nginx/debian"
+CONF_PATH="/etc/nginx/sites-available/apt-proxy"
+MIRROR_URL="https://mirror.sg.gs"
+SERVER_IP="10.0.0.41"
+
+apt update && apt install nginx -y
+
+mkdir -p "$CACHE_DIR"
+chown www-data:www-data "$CACHE_DIR"
+
+cat <<EOF > "$CONF_PATH"
+# Cache definition
+proxy_cache_path $CACHE_DIR 
+    levels=1:2 
+    keys_zone=deb_cache:10m 
+    max_size=5g 
+    inactive=14d 
+    use_temp_path=off;
+
+server {
+    listen 80;
+    server_name $SERVER_IP;
+
+    # Logging
+    access_log /var/log/nginx/apt-proxy-access.log;
+    error_log /var/log/nginx/apt-proxy-error.log;
+
+    location / {
+        proxy_pass $MIRROR_URL;
+        proxy_cache deb_cache;
+        
+        # Metadata (Release, InRelease, Packages) - Refresh every 6 hours
+        proxy_cache_valid 200 302 6h;
+        
+        # Deb packages (.deb) - Cache for 14 days
+        location ~* \.deb$ {
+            proxy_pass $MIRROR_URL;
+            proxy_cache_valid 200 302 14d;
+        }
+
+        # Header to see if it's a HIT or MISS
+        add_header X-Cache-Status \$upstream_cache_status;
+
+        # Ensure correct SNI and Host for the backend HTTPS mirror
+        proxy_set_header Host mirror.sg.gs;
+        proxy_ssl_server_name on;
+        proxy_ssl_verify on;
+        proxy_ssl_trusted_certificate /etc/ssl/certs/ca-certificates.crt;
+    }
+}
+EOF
+
+rm -f /etc/nginx/sites-enabled/default
+
+ln -sf "$CONF_PATH" /etc/nginx/sites-enabled/
+
+if nginx -t; then
+    systemctl restart nginx
+    echo "--- NGINX REVERSE CACHING PROXY IS RUNNING ---"
+else
+    echo "[!] FAILED [!]"
+    exit 1
+fi
