bb8b3058611f8a59db17f9fa95f5527520f542ca
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-02-28 06:16:16 +0800

    Update lxc-nginx-reverse-caching-proxy.sh

--- lxc-nginx-reverse-caching-proxy.sh
+++ lxc-nginx-reverse-caching-proxy.sh
@@ -25,48 +25,29 @@ apt clean
 apt autoremove -y
 
 # NGINX REVERSE CACHING PROXY
-set -e
-
-if [ "$EUID" -ne 0 ]; then 
-  echo "Please run as root"
-  exit 1
-fi
-
 CACHE_DIR="/var/cache/nginx/debian"
 CONF_PATH="/etc/nginx/sites-available/apt-proxy"
 MIRROR_URL="https://mirror.sg.gs"
 SERVER_IP="10.0.0.41"
 
 apt update && apt install nginx -y
-
 mkdir -p "$CACHE_DIR"
 chown www-data:www-data "$CACHE_DIR"
 
 tee "$CONF_PATH" <<EOF
-proxy_cache_path $CACHE_DIR
-    levels=1:2
-    keys_zone=deb_cache:10m
-    max_size=5g
-    inactive=30d
-    use_temp_path=off;
+proxy_cache_path $CACHE_DIR levels=1:2 keys_zone=deb_cache:10m max_size=5g inactive=30d use_temp_path=off;
 server {
     listen 80;
     server_name $SERVER_IP;
-    access_log /var/log/nginx/apt-proxy-access.log;
-    error_log /var/log/nginx/apt-proxy-error.log;
+    access_log /var/log/nginx/apt-proxy.log;
+    error_log off;
     location / {
         proxy_pass $MIRROR_URL;
         proxy_cache deb_cache;
         proxy_cache_valid 200 302 6h;
-        location ~* \.deb$ {
-            proxy_pass $MIRROR_URL;
-            proxy_cache_valid 200 302 14d;
-        }
-        add_header X-Cache-Status \$upstream_cache_status;
         proxy_set_header Host mirror.sg.gs;
         proxy_ssl_server_name on;
-        proxy_ssl_verify on;
-        proxy_ssl_trusted_certificate /etc/ssl/certs/ca-certificates.crt;
+        add_header X-Cache-Status \$upstream_cache_status;
     }
 }
 EOF
