be32827e85b7081e2d41bdd6f918c1dfdb376d3d
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-03-02 21:37:26 +0800

    Update nginx-reverse-caching-proxy.sh

--- nginx-reverse-caching-proxy.sh
+++ nginx-reverse-caching-proxy.sh
@@ -35,7 +35,7 @@ mkdir -p "$CACHE_DIR"
 chown www-data:www-data "$CACHE_DIR"
 
 tee "$CONF_PATH" <<EOF
-proxy_cache_path $CACHE_DIR levels=1:2 keys_zone=deb_cache:10m max_size=5g inactive=30d use_temp_path=off;
+proxy_cache_path $CACHE_DIR levels=1:2 keys_zone=deb_cache:10m max_size=5g inactive=180d use_temp_path=off;
 server {
     listen 80;
     server_name $SERVER_IP;
@@ -44,7 +44,9 @@ server {
     location / {
         proxy_pass $MIRROR_URL;
         proxy_cache deb_cache;
-        proxy_cache_valid 200 302 6h;
+        proxy_cache_valid 200 302 1h;
+        proxy_cache_lock on;
+        proxy_cache_use_stale error timeout invalid_header updating http_500 http_502 http_503 http_504;
         proxy_set_header Host mirror.sg.gs;
         proxy_ssl_server_name on;
         add_header X-Cache-Status \$upstream_cache_status;
