2b59f58b493fcd46049ba0fd0ea2b94c5cfbdea5
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-03-22 00:12:31 +0800

    Update oggyproduction.sh

--- oggyproduction.sh
+++ oggyproduction.sh
@@ -176,4 +176,4 @@ else
 fi
 EOF
 chmod 755 -v ./update.sh
-sudo bash -c "(crontab -l 2>/dev/null; echo '0 5 * * 3 /home/oggy/update.sh > /home/oggy/update.log 2>&1') | crontab -"
+sudo bash -c "(crontab -l 2>/dev/null; echo '30 6 * * 1 /home/oggy/update.sh > /home/oggy/update.log 2>&1') | crontab -"
