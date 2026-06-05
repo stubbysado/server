51554d9e0e0014d7e7985ff1cc59d50122530f97
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-06-06 04:38:14 +0800

    Add files via upload

---
+++ 109-qbittorrent-nox.sh
@@ -0,0 +1,95 @@
+#!/bin/bash -x
+
+# REMOVE SOURCES.LIST
+sudo rm -f /etc/apt/sources.list
+
+# SOURCES.LIST
+sudo tee /etc/apt/sources.list.d/debian.sources <<'EOF'
+Types: deb deb-src
+URIs: http://10.0.0.41/debian
+Suites: trixie trixie-updates
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+Types: deb deb-src
+URIs: http://10.0.0.41/debian-security
+Suites: trixie-security
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+EOF
+
+# PACKAGES
+sudo apt update
+sudo apt upgrade -y
+sudo apt clean
+sudo apt autoremove -y
+
+# NFS
+sudo apt update
+sudo apt install nfs-common -y
+sudo mkdir -p /mnt/server
+sudo chown oggy:oggy /mnt/server
+echo "10.0.0.21:/mnt/server /mnt/server nfs rw,async,nconnect=8,rsize=1048576,wsize=1048576,noatime,nofail,noauto 0 0" | sudo tee -a /etc/fstab
+sudo mount /mnt/server
+
+sudo tee /etc/systemd/system/nfs-mount.service <<'EOF'
+[Unit]
+Description=NFS Mount 10.0.0.21:/mnt/server
+After=network-online.target nfs-client.target
+Wants=network-online.target
+Before=remote-fs.target shutdown.target
+StartLimitBurst=10
+StartLimitIntervalSec=90
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/bin/mount /mnt/server
+ExecStop=/bin/umount -l -f /mnt/server
+Restart=on-failure
+RestartSec=6
+TimeoutStartSec=15
+TimeoutStopSec=15
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable nfs-mount.service
+
+# QBITTORRENT-NOX
+wget https://github.com/userdocs/qbittorrent-nox-static/releases/download/release-5.2.1_v2.0.12/x86_64-qbittorrent-nox
+sudo mv /home/oggy/x86_64-qbittorrent-nox /usr/local/bin/qbittorrent-nox
+sudo chmod 755 /usr/local/bin/qbittorrent-nox
+
+sudo tee /etc/systemd/system/qbittorrent-nox.service <<'EOF'
+[Unit]
+Description=qBittorrent-nox service
+After=network.target
+
+[Service]
+User=oggy
+Group=oggy
+ExecStart=/usr/local/bin/qbittorrent-nox
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable qbittorrent-nox
+sudo systemctl start qbittorrent-nox
+
+# ZRAM
+sudo apt update
+sudo apt install systemd-zram-generator -y
+
+echo "[zram0]
+zram-size = ram" | sudo tee /etc/systemd/zram-generator.conf
+
+echo "vm.swappiness = 180
+vm.watermark_boost_factor = 0
+vm.watermark_scale_factor = 125
+vm.page-cluster = 0" | sudo tee /etc/sysctl.d/99-zram.conf
