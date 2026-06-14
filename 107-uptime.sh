b6e8ce358045851168c3a67e32fae647a6e16c5e
stubbysado <114629686+stubbysado@users.noreply.github.com>
2026-06-15 07:12:36 +0800

    Update 107-uptime.sh

--- 107-uptime.sh
+++ 107-uptime.sh
@@ -24,6 +24,9 @@ apt upgrade -y
 apt clean
 apt autoremove -y
 
+# ENABLE SSH
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config && systemctl restart ssh.service
+
 # TIMEZONE
 timedatectl set-timezone Asia/Kuala_Lumpur
 
